9bc3233c88bcbde036becbd4b4b1daf8862c6dbc
iobami <ayobamialadenoye@yahoo.com>
2024-10-14 12:31:09 +0100

    ui center fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,33 @@ import Link from 'next/link'
 
 export default function Home () {
   return (
-    <div className="flex flex-col gap-8">
-      <h3 className="app_survey__title">
-        Welcome to Our Automotive Sales Survey!
-      </h3>
+    <div className="app_survey_container">
+      <div className="app_survey_container__upper">
+        <div className="flex flex-col gap-8">
+          <h3 className="app_survey__title">
+            Welcome to Our Automotive Sales Survey!
+          </h3>
 
-      <p className="app_survey__info">
-        We appreciate your time and feedback as we work to better understand our
-        customers&apos; needs. This survey is designed to be quick and easy,
-        with no interruptions or delays, so you can share your thoughts
-        seamlessly.
-      </p>
+          <p className="app_survey__info">
+            We appreciate your time and feedback as we work to better understand
+            our customers&apos; needs. This survey is designed to be quick and
+            easy, with no interruptions or delays, so you can share your
+            thoughts seamlessly.
+          </p>
 
-      <div className="flex gap-4">
-        <Link href={'/survey'}>
-          <Button
-            size="md"
-            backgroundColor="shark-950"
-            className="app_survey__btn"
-            // onClick={handleRestart}
-          >
-            Start survey
-          </Button>
-        </Link>
+          <div className="flex gap-4">
+            <Link href={'/survey'}>
+              <Button
+                size="md"
+                backgroundColor="shark-950"
+                className="app_survey__btn"
+                // onClick={handleRestart}
+              >
+                Start survey
+              </Button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
